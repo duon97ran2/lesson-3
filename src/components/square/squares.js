f74fd63faf0e10f90ff8f3f2a12b4c417841036e
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-07-01 21:10:19 +0700

    update tic-tac-toe-game

--- src/components/square/squares.js
+++ src/components/square/squares.js
@@ -1,9 +1,19 @@
-import { useState } from "react"
 import style from "./square.module.css"
 
-const Square = () => {
-  const [status, setStatus] = useState(false);
-  return <button onClick={() => { setStatus(!status) }} className={style.square} >{status ? "x" : ""}</button>
+
+const Square = ({ value, handlePlay, winner }) => {
+  function setColor(value) {
+    if (winner) {
+      return "green"
+    }
+    if (value == "x") {
+      return "red"
+    }
+    if (value == "o") {
+      return "blue"
+    }
+  }
+  return <button className={style.square} onClick={handlePlay} style={{ background: setColor(value, winner) }} >{value}</button>
 }
 
 export default Square
